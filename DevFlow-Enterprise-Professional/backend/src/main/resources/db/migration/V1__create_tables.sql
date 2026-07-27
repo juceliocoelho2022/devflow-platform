ef63abdd8550098032c726e3cb903bd5b38df1a2
Jucelio Farias Coelho <juceliocoelho2010@gmail.com>
2026-07-27 11:35:01 -0300

    feat(database): create initial Flyway migration

--- DevFlow-Enterprise-Professional/backend/src/main/resources/db/migration/V1__create_tables.sql
+++ DevFlow-Enterprise-Professional/backend/src/main/resources/db/migration/V1__create_tables.sql
@@ -1,1 +1,17 @@
--- Initial migration
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE users (
+                       id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+                       full_name VARCHAR(150) NOT NULL,
+
+                       email VARCHAR(150) NOT NULL UNIQUE,
+
+                       password VARCHAR(255) NOT NULL,
+
+                       active BOOLEAN NOT NULL DEFAULT TRUE,
+
+                       created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+                       updated_at TIMESTAMP
+);
